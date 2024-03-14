4f5ba11153c501b569a1e0f73fb778e0f5678a63
xingbowei <72482498+xingbowei1@users.noreply.github.com>
2024-03-14 17:25:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## A personal profile made by Github Pages.
+
+For more details please visit [Home | Bowei Xing ](https://xingbowei1.github.io/).
+
---
+++ user/themes/asgard/js/site.js
@@ -0,0 +1,58 @@
+var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
+
+function scrollHeader() {
+    // Has scrolled class on header
+    var zvalue = $(document).scrollTop();
+    if ( zvalue > 75 )
+        $("#header").addClass("scrolled");
+    else
+        $("#header").removeClass("scrolled");
+}
+
+function parallaxBackground() {
+    $('.parallax').css('background-positionY', ($(window).scrollTop() * 0.3) + 'px');
+}
+
+jQuery(document).ready(function($){
+
+    scrollHeader();
+
+    // Scroll Events
+    if (!isTouch){
+        $(document).scroll(function() {
+            scrollHeader();
+            parallaxBackground();
+        });
+    };
+
+    // Touch scroll
+    $(document).on({
+        'touchmove': function(e) {
+            scrollHeader(); // Replace this with your code.
+        }
+    });
+
+    //Smooth scroll to start
+    $('#to-start').click(function(){
+        var start_y = $('#start').position().top;
+        var header_offset = 45;
+        window.scroll({ top: start_y - header_offset, left: 0, behavior: 'smooth' });
+        return false;
+    });
+
+    //Smooth scroll to top
+    $('#to-top').click(function(){
+        window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+        return false;
+    });
+
+    // Tree Menu
+    $(".tree").treemenu({delay:300});
+
+    $(document).on('click', '.bib-url', function (event) {
+        event.preventDefault();
+        target_bib = $(this).parents(".post-middle").find(".bib");
+        target_bib.toggle();
+    });
+
+});
